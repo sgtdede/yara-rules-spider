ea7ab5755d476553b59eeb960bde1f93697b47cc
sgtdede <sgtdede@gmail.com>
2021-08-26 23:55:28 +0200

    refacto

--- yara-rules-spider.py
+++ yara-rules-spider.py
@@ -123,39 +123,10 @@ def one_rule_to_rule_them_all(compile=True):
         repo_rules.save(ultimate_compiled_rule_path)
 
 
-spider(args.repositories)
-one_rule_to_rule_them_all()
+def main():
+    spider(args.repositories)
+    one_rule_to_rule_them_all()
 
 
-
-
-def fetch_valhalla_rules():
-    v = ValhallaAPI()
-    status = v.get_status()
-    valhalla_rules_latest_version = str(status["version"])
-    valhalla_rules_path = join(get_default_root(),'valhalla-rules.yar')
-    valhalla_rules_version_path = join(get_default_root(),'valhalla-rules-versions')
-    valhalla_rules_compiled_path = join(get_default_root(),'valhalla-rules.compiled')
-
-    if isfile(valhalla_rules_path) and isfile(valhalla_rules_compiled_path):
-        try:
-            with open(valhalla_rules_version_path, 'r') as vvf:
-                valhalla_rules_current_version = vvf.read()
-            if valhalla_rules_current_version == valhalla_rules_latest_version:
-                print("Valhalla rules are already up to date")
-                return yara.load(valhalla_rules_compiled_path)
-            else:
-                print("Valhalla rules are outdated")
-
-        except FileNotFoundError:
-            pass
-
-    print("Fetching latest valhalla rules....")
-    rules = v.get_rules_text()
-    with open(valhalla_rules_path, 'w') as fh:
-        fh.write(rules)
-    with open(valhalla_rules_version_path, 'w') as vvf:
-        vvf.write(valhalla_rules_latest_version)
-    rules = yara.compile(filepath=valhalla_rules_path)
-    rules.save(valhalla_rules_compiled_path)
-    return rules
+if __name__ == "__main__":
+    main()
